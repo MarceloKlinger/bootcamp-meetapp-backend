c3b9e9ad75e38c41ab093ff3465696b267af492e
Marcelo Klinger <marcelo.klinger@hotmail.com>
2019-10-03 09:16:46 -0300

    finishing challenge 2

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,22 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation is fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -16,7 +28,7 @@ class SessionController {
 
     // check if password is registered
     if (!(await user.checkPassword(password))) {
-      return res.status(4001).json({ error: 'Password does not match' });
+      return res.status(401).json({ error: 'Password does not match' });
     }
 
     const { id, name } = user;
